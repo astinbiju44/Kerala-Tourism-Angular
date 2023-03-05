49569a541d3cbd8973d01a582c641537041223d3
Astin Biju <astinbiju44@gmail.com>
2023-03-05 14:53:51 +0530

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DistrictComponent } from './pages/district/district.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './utilites/header/header.component';
+import { NavbarComponent } from './utilites/navbar/navbar.component';
+import { FooterComponent } from './utilites/footer/footer.component';
+import { SinglepageComponent } from './singlepage/singlepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DistrictComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent,
+    SinglepageComponent
   ],
   imports: [
     BrowserModule,
